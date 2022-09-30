5adc4c601c30712b56e176657cb1938913795f7b
JoseMariaGarciaMarquez <30852961+JoseMariaGarciaMarquez@users.noreply.github.com>
2022-09-30 12:50:27 -0500

    Filtros de señales discretas usando fft y fftshift
    Estos filtros usan fft y fftshift de la libreria numpy

---
+++ PasaAltas.py
@@ -0,0 +1,59 @@
+#Diseño de filtros---------------------------------------------------------------------------------
+
+#----Filtro-Pasa-Altas-----------------------------------------------------------------------------
+#----INICIO----------------------------------------------------------------------------------------
+def PA_JMGM(t,FC,señal):
+#--------Transformar la señal recibida-------------------------------------------------------------
+    señal_ff = fftshift(fft(señal))
+#--------Crear el dominio temporal-----------------------------------------------------------------
+    N = len(t)
+    dt = t[1]-t[0]
+#--------Crear el dominio de Fourier---------------------------------------------------------------
+    FN = 1/(2*dt)
+    F0 = 1/(N*dt)
+
+    F = np.arange(-FN,FN,F0)
+
+#--------Crear el filtro---------------------------------------------------------------------------    
+    HighPass = [0]*len(F)
+    for i in range (len(HighPass)):
+        if abs(F[i]) >= FC:
+            HighPass[i] = 1
+#--------Filtrar en el dominio de Fourier----------------------------------------------------------
+    señal_f = []
+    for i in range (len(HighPass)):
+        señal_f.append(señal_ff[i]*HighPass[i])
+
+#--------Reconstruir la señal con las amplitudes filtradas---------------------------------------- 
+    señal_r = ifft(ifftshift(señal_f))
+
+#--------Gráficas----------------------------------------------------------------------------------
+    fig, ax = plt.subplots(2,2, figsize = (15,10), dpi = 150)
+    ax[0,0].plot(t,señal)
+    ax[0,0].set_title("Señal original")
+    ax[0,0].set_xlabel("Radiación")
+    ax[0,0].set_ylabel("Años")
+    ax[0,0].grid()
+    
+    ax[0,1].plot(F,señal_ff, c = 'red')
+    ax[0,1].scatter(F,señal_ff, c = 'salmon')
+    ax[0,1].set_title("Espectro de amplitud")
+    ax[0,1].set_xlabel("Frecuencia [años^-1]")
+    ax[0,1].set_ylabel("Amplitud")
+    ax[0,1].grid()
+    
+    ax[1,0].plot(F, HighPass)
+    ax[1,0].set_title("Filtro pasa-altas")
+    ax[1,0].grid()
+    
+    ax[1,1].plot(t,señal_r)
+    ax[1,1].set_title("Señal filtrada")
+    ax[1,1].set_title("Señal original")
+    ax[1,1].set_xlabel("Radiación")
+    ax[1,1].set_ylabel("Años")
+    ax[1,1].grid()
+    
+    fig.suptitle("FILTRO PASA-ALTAS", fontsize = "20")
+    
+    return fig,ax
+#----FIN-------------------------------------------------------------------------------------------
